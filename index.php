305cf5b6535b6273242c78a071f9c7425ccad826
Eliot <eliotchill99@gmail.com>
2020-01-30 16:45:57 +0000

    Initial holding page

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+    <title>Simulated Annealing</title>    
+    <link rel="stylesheet" type="text/css" href="style.css"><link href="https://fonts.googleapis.com/css?family=Francois+One&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Simulated Annealing - Coming soon!</h1>
+    </body>
+</html>
