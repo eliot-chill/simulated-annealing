43b7603d51ed7c8c213b71f00971a0cd74e92646
Eliot Cholerton-Hill <eliotchill99@gmail.com>
2020-01-31 18:11:01 +0000

    Working graph

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-    <title>Simulated Annealing</title>    
-    <link rel="stylesheet" type="text/css" href="style.css"><link href="https://fonts.googleapis.com/css?family=Francois+One&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <h1>Simulated Annealing - Coming soon!</h1>
-    </body>
-</html>
